2eff204940c0be4c205e734fc6c2295d9d792b17
rafael_valero <rafaelvalerofernandez@gmail.com>
2020-03-21 18:34:33 +0000

    update arimas for 3 days predictions

--- covid_forecast/utils/visualizations.py
+++ covid_forecast/utils/visualizations.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def plt_arima_forecast(y, forecasts, conf_int=False,
-                       lenght_for_training=5,
                        title='Country name here',
                        y_label='Deaths',
                        x=None,
@@ -17,16 +16,49 @@ def plt_arima_forecast(y, forecasts, conf_int=False,
     :param save_here: str where to save.
     :return:
     """
+    lenght_for_forecast = forecasts.__len__()
     plt.clf()
     if x is None:
         x = np.arange(y.shape[0])
     plt.plot(x, y, 'b*--', label='Real')
     plt.plot(x[lenght_for_training:], forecasts, 'go--', label='Forecast')
-    plt.xlabel('time')
+    plt.xlabel('Date')
     plt.title(title)
     plt.ylabel(y_label)
     if conf_int is not False:
         plt.fill_between(x[lenght_for_training:],
+                         conf_int[:, 0], conf_int[:, 1],
+                         alpha=0.1, color='b')
+    plt.legend(loc='upper left')
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    plt.savefig(save_here)
+    plt.clf()
+    return None
+
+def plt_arima_forecast_outsample(y, forecasts, conf_int=False,
+                       title='Country name here',
+                       y_label='Deaths',
+                       x=None,
+                       save_here='arima_case.png'):
+    """
+    :param y: real vualues
+    :param forecast: predicted values
+    :param lenght_for_training: like 90% lenght of y
+    :param save_here: str where to save.
+    :return:
+    """
+    lenght_for_forecast = forecasts.__len__()
+    plt.clf()
+    if x is None:
+        x = np.arange(y.shape[0])
+    plt.plot(x[:y.__len__()], y, 'b*--', label='Real')
+    plt.plot(x[-lenght_for_forecast:], forecasts, 'go--', label='Forecast')
+    plt.xlabel('Date')
+    plt.title(title)
+    plt.ylabel(y_label)
+    if conf_int is not False:
+        plt.fill_between(x[-lenght_for_forecast:],
                          conf_int[:, 0], conf_int[:, 1],
                          alpha=0.1, color='b')
     plt.legend(loc='upper left')

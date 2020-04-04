f45ad18684ba554677e3bb20b04e6ea9eef8bb77
rafael_valero <rafaelvalerofernandez@gmail.com>
2020-04-04 13:49:13 +0100

    working SIR bases

---
+++ analysis/estimate_sir_params_v2.py
@@ -0,0 +1,148 @@
+"""Credit to: Lewuathe (Github). Source code: https://github.com/Lewuathe/COVID19-SIR"""
+import os
+# import re
+import sys
+from datetime import timedelta, datetime
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+# from IPython.display import display
+from scipy.integrate import solve_ivp
+from scipy.optimize import minimize
+
+main_dir = os.path.abspath(os.pardir)
+sys.path.insert(0, main_dir)
+
+fig_export_path = os.path.join(main_dir, "reports", "estimate_sir_params")
+if not os.path.isdir(fig_export_path):
+    os.mkdir(fig_export_path)
+fig_export_path += os.sep
+
+result_df_vnames = ["country", "beta", "gamma", "r_0"]
+country_region_vname = 'Country/Region'
+s_0_vname = "s_0"
+population_df_vnames = [country_region_vname, s_0_vname]
+beta, gamma = 0.001, 0.001
+
+
+class Learner(object):
+    def __init__(self, country, loss_funct, df_dct, predict_range, i_0, r_0):
+        self.country = country
+        self.loss = loss_funct
+        self.df_dct = df_dct
+        self.predict_range = predict_range
+        self.i_0 = i_0
+        self.r_0 = r_0
+
+    def load_df(self, country):
+        confirmed_df_name = "confirmed_global"
+        df_dct_tmp = self.df_dct.copy()
+        vars_to_use = df_dct_tmp[list(df_dct_tmp.keys())[0]].drop(columns=country_region_vname)
+        for df_name in df_dct_tmp.keys():
+            if "confirmed" in df_name:
+                df_dct_tmp[df_name] = df_dct_tmp[df_name][df_dct_tmp[df_name][country_region_vname] == country]
+                s_0 = int(df_dct_tmp[df_name][s_0_vname].iloc[0])
+                df_dct_tmp[df_name] = df_dct_tmp[df_name].iloc[0].drop(index=[country_region_vname, s_0_vname])
+                vars_to_use = np.trim_zeros(df_dct_tmp[df_name], trim="fb").index  # trims head and tail of series 0s
+                df_dct_tmp[df_name] = df_dct_tmp[df_name][vars_to_use]
+                confirmed_df_name = df_name
+                break
+        for df_name in [df_name_tmp for df_name_tmp in df_dct_tmp.keys() if df_name_tmp != confirmed_df_name]:
+            df_dct_tmp[df_name] = df_dct_tmp[df_name][df_dct_tmp[df_name][country_region_vname] == country]
+            df_dct_tmp[df_name] = df_dct_tmp[df_name].iloc[0].drop(index=[country_region_vname])
+            df_dct_tmp[df_name] = df_dct_tmp[df_name][vars_to_use]
+        return df_dct_tmp, s_0
+
+    def extend_index(self, index, new_size):
+        values = index.values
+        current = datetime.strptime(index[-1], '%m/%d/%y')
+        while len(values) < new_size:
+            current = current + timedelta(days=1)
+            values = np.append(values, datetime.strftime(current, '%m/%d/%y'))
+        return values
+
+    def train(self, plot=False):
+        global beta
+        global gamma
+        df_dct_tmp, s_0 = self.load_df(self.country)
+
+        recovered = df_dct_tmp[[df_name for df_name in df_dct_tmp.keys() if "recovered" in df_name][0]]
+        deaths = df_dct_tmp[[df_name for df_name in df_dct_tmp.keys() if "deaths" in df_name][0]]
+        confirmed = df_dct_tmp[[df_name for df_name in df_dct_tmp.keys() if "confirmed" in df_name][0]]
+        confirmed = confirmed - recovered - deaths
+        # i_0 = confirmed.iloc[0]
+
+        print(self.country, "population:", s_0, "start date", confirmed.index[0])  #, "infected:", i_0)
+        # print(len(confirmed) == len(deaths) == len(recovered))
+        optimal = minimize(loss, [0.001, 0.001], args=(confirmed, recovered, s_0, self.i_0, self.r_0),
+                           method='L-BFGS-B', bounds=[(0.00000001, 0.4), (0.00000001, 0.4)] ,
+                           options={'maxiter': 5})
+        # print(optimal)
+        beta, gamma = optimal.x
+
+        if plot:
+            # Plotting prep
+            new_index, extended_actual, extended_recovered, extended_death, prediction = self.predict(
+                beta, gamma, confirmed, recovered, deaths, self.country, s_0, self.i_0, self.r_0)
+            df = pd.DataFrame(
+                {'Infected (actual)': extended_actual, 'Recovered (actual)': extended_recovered,
+                 'Death (actual)': extended_death, 'Susceptible': prediction.y[0], 'Infected': prediction.y[1],
+                 'Recovered': prediction.y[2]}, index=new_index)
+
+            # Actual plotting
+            fig, ax = plt.subplots(figsize=(15, 10))
+            ax.set_title(self.country)
+            df.plot(ax=ax)
+
+            # Exporting plot
+            # export_str = re.sub(r'[^A-Za-z0-9 ]+', '', self.country)
+            fig.savefig(fig_export_path+f"{self.country}.png")
+            plt.close(fig)
+
+        r_0 = (beta / gamma)
+        print(f"country={self.country}, beta={beta:.8f}, gamma={gamma:.8f}, r_0={r_0:.8f}", "\n")
+        return pd.Series([self.country, beta, gamma, r_0], index=result_df_vnames)
+
+    def predict(self, beta, gamma, confirmed, recovered, deaths, country, s_0, i_0, r_0):
+        new_index = self.extend_index(confirmed.index, self.predict_range)
+        size = len(new_index)
+
+        def SIR(t, y):
+            S = y[0]
+            I = y[1]
+            R = y[2]
+            return [-beta * S * I, beta * S * I - gamma * I, gamma * I]
+
+        extended_actual = np.concatenate((confirmed.values, [None] * (size - len(confirmed.values))))
+        extended_recovered = np.concatenate((recovered.values, [None] * (size - len(recovered.values))))
+        extended_death = np.concatenate((deaths.values, [None] * (size - len(deaths.values))))
+        return new_index, extended_actual, extended_recovered, extended_death, solve_ivp(
+            SIR, [0, size], [s_0, i_0, r_0], t_eval=np.arange(0, size, 1))
+
+
+def loss(point, confirmed, recovered, s_0, i_0, r_0):
+    size = len(confirmed)
+    beta, gamma = point
+
+    def SIR(t, y):
+        S = y[0]
+        I = y[1]
+        R = y[2]
+        return [-beta*S*I, beta*S*I-gamma*I, gamma*I]
+
+    solution = solve_ivp(SIR, [0, size], [s_0, i_0, r_0], t_eval=np.arange(0, size, 1), vectorized=True)
+    l1 = np.sqrt(np.mean((solution.y[1] - confirmed)**2))
+    l2 = np.sqrt(np.mean((solution.y[2] - recovered)**2))
+    alpha = 0.1
+    return alpha * l1 + (1 - alpha) * l2
+
+
+def estimate_sir_params(countries, df_dct, predict_range=365, i_0=2, r_0=0, plot=False):
+
+    results_df = pd.DataFrame(columns=result_df_vnames)
+
+    for country in countries:
+        learner = Learner(country, loss, df_dct, predict_range, i_0, r_0)
+        results_df = results_df.append(learner.train(plot=plot), ignore_index=True, sort=False)
+    return results_df
---
+++ analysis/solver_v3.py
@@ -0,0 +1,235 @@
+"""
+https://www.maa.org/press/periodicals/loci/joma/the-sir-model-for-spread-of-disease-the-differential-equation-model
+https://scipython.com/book/chapter-8-scipy/additional-examples/the-sir-epidemic-model/
+
+:TODO transform dates
+"""
+
+
+
+
+# !/usr/bin/python
+import numpy as np
+import pandas as pd
+from scipy.integrate import solve_ivp
+from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+from datetime import timedelta, datetime
+import argparse
+import sys
+import json
+import ssl
+import urllib.request
+import os
+
+
+"""Here some parameters to play around"""
+countries = ['United Kingdom','Spain','US','Italy','Cyprus', 'France', 'India']
+download = False
+plot_grpahs = True
+countries_populations = {'United Kingdom':66488991,
+                         'Spain':46723749 ,
+                         'US': 327167434,'Italy':60431283 ,
+                         'Cyprus':1189265,
+                         'France':66900000,
+                         'China': 1386000000,
+                         'India': 1339000000}
+OUTPUT = 'output'
+os.makedirs(OUTPUT,exist_ok=True)
+"""------------------------------------------------"""
+
+def remove_province(input_file, output_file):
+    input = open(input_file, "r")
+    output = open(output_file, "w")
+    output.write(input.readline())
+    for line in input:
+        if line.lstrip().startswith(","):
+            output.write(line)
+    input.close()
+    output.close()
+
+
+def download_data(url_dictionary):
+    # Lets download the files
+    for url_title in url_dictionary.keys():
+        urllib.request.urlretrieve(url_dictionary[url_title], "./data/" + url_title)
+
+
+def load_json(json_file_str):
+    # Loads  JSON into a dictionary or quits the program if it cannot.
+    try:
+        with open(json_file_str, "r") as json_file:
+            json_variable = json.load(json_file)
+            return json_variable
+    except Exception:
+        sys.exit("Cannot open JSON file: " + json_file_str)
+
+
+class Learner(object):
+    def __init__(self, country, loss,s_0 = 1, i_0=1, r_0=1, predict_range=300):
+        self.country = country
+        self.loss = loss
+        #self.start_date = start_Data
+        self.start_date = self.get_starting_date()
+        print(self.start_date )
+        self.predict_range = predict_range
+        self.s_0 = s_0
+        self.i_0 = i_0
+        self.r_0 = r_0
+    def  get_starting_date(self):
+        """
+        This function help you with the starting date
+        :param country:
+        :return:
+        """
+        df = pd.read_csv('data/time_series_19-covid-Confirmed-country.csv')
+        country_df = df[df['Country/Region'] == self.country]
+        # Remove values no related to date
+        country_df = country_df.iloc[:, 4:]
+        length_array = country_df.shape[1]
+        remove_initial_zeros = np.trim_zeros(country_df.iloc[:, 4:].values[0]).__len__()
+        # Select start date as well
+        return country_df.iloc[:, length_array - remove_initial_zeros].name
+
+    def load_confirmed(self, country):
+        df = pd.read_csv('data/time_series_19-covid-Confirmed-country.csv')
+        country_df = df[df['Country/Region'] == country]
+        return country_df.iloc[0].loc[self.start_date:]
+
+    def load_recovered(self, country):
+        df = pd.read_csv('data/time_series_19-covid-Recovered-country.csv')
+        country_df = df[df['Country/Region'] == country]
+        return country_df.iloc[0].loc[self.start_date:]
+
+    def load_dead(self, country):
+        df = pd.read_csv('data/time_series_19-covid-Deaths-country.csv')
+        country_df = df[df['Country/Region'] == country]
+        return country_df.iloc[0].loc[self.start_date:]
+
+    def extend_index(self, index, new_size):
+        values = index.values
+        current = datetime.strptime(index[-1], '%m/%d/%y')
+        while len(values) < new_size:
+            current = current + timedelta(days=1)
+            values = np.append(values, datetime.strftime(current, '%m/%d/%y'))
+        return values
+
+    def predict(self, beta, gamma, data, recovered, death, country, s_0, i_0, r_0):
+        new_index = self.extend_index(data.index, self.predict_range)
+        size = len(new_index)
+
+        def SIR(t, y):
+            S = y[0]
+            I = y[1]
+            R = y[2]
+            #return [-beta * S * I / s_0, beta * S * I /s_0 - gamma * I, gamma * I]
+            return [-beta * S * I, beta * S * I - gamma * I, gamma * I]
+
+        extended_actual = np.concatenate((data.values, [None] * (size - len(data.values))))
+        extended_recovered = np.concatenate((recovered.values, [None] * (size - len(recovered.values))))
+        extended_death = np.concatenate((death.values, [None] * (size - len(death.values))))
+        return new_index, extended_actual, extended_recovered, \
+               extended_death, solve_ivp(SIR, [0, size],
+                             [s_0, i_0, r_0],
+                             t_eval=np.arange(0, size, 1),method='LSODA')
+                             #t_eval=np.arange(0, size, 1), method='LSODA')
+
+    def train(self):
+        self.recovered = self.load_recovered(self.country)
+        self.death = self.load_dead(self.country)
+        self.data = (self.load_confirmed(self.country) - self.recovered - self.death)
+
+        optimal = minimize(loss, [0.2/self.s_0, 0.1/self.s_0], args=(self.data, self.recovered, self.s_0, self.i_0, self.r_0),
+                           method='L-BFGS-B', bounds=[(0.00000001, 2), (0.00000001, 0.1)])
+        print(optimal)
+        beta, gamma = optimal.x
+        new_index, extended_actual, extended_recovered, extended_death,\
+                                prediction = self.predict(beta, gamma, self.data,
+                                                          self.recovered, self.death,
+                                                          self.country,
+                                                          self.s_0, self.i_0,
+                                                          self.r_0)
+        df = pd.DataFrame(
+            {'Infected data': extended_actual, 'Recovered data': extended_recovered,
+             'Death data': extended_death,
+             'Susceptible': prediction.y[0], 'Infected': prediction.y[1], 'Recovered': prediction.y[2]},
+            index=new_index)
+        # Changes the dates format
+        df.reset_index(inplace=True)
+        df['dates'] = pd.to_datetime(df['index'])
+        del df['index']
+        df.set_index('dates', inplace=True)
+        plt.figure()
+        fig, ax = plt.subplots(figsize=(10, 7))
+        ax.set_title(self.country)
+        df.plot(ax=ax)
+        fig.savefig(f"output/{self.country}.png")
+        if plot_grpahs:
+            plt.gcf()
+            plt.show()
+        else:
+            plt.close('all')
+        print(f"country={self.country}, beta={beta:.8f}, gamma={gamma:.8f}, r_0:{(beta / gamma):.8f}")
+
+        return df
+
+
+def loss(point, data, recovered, s_0, i_0, r_0):
+    size = len(data)
+    beta, gamma = point
+
+    def SIR(t, y):
+        S = y[0]
+        I = y[1]
+        R = y[2]
+        #return [-beta * S * I / s_0, beta * S * I /s_0 - gamma * I, gamma * I]
+        return [-beta * S * I , beta * S * I  - gamma * I, gamma * I]
+
+    solution = solve_ivp(SIR, [0, size], [s_0, i_0, r_0], t_eval=np.arange(0, size, 1),
+                         vectorized=True,method='LSODA')
+                         #vectorized=True)
+
+    l1 = np.sqrt(np.mean((solution.y[1] - data) ** 2))
+    l2 = np.sqrt(np.mean((solution.y[2] - recovered) ** 2))
+    alpha = 0.9
+    return alpha * l1 + (1 - alpha) * l2
+
+
+def main():
+    countries, download, startdate, predict_range, s_0, i_0, r_0 = parse_arguments()
+
+    if download:
+        data_d = load_json("./data_url.json")
+        download_data(data_d)
+
+    remove_province('data/time_series_19-covid-Confirmed.csv', 'data/time_series_19-covid-Confirmed-country.csv')
+    remove_province('data/time_series_19-covid-Recovered.csv', 'data/time_series_19-covid-Recovered-country.csv')
+    remove_province('data/time_series_19-covid-Deaths.csv', 'data/time_series_19-covid-Deaths-country.csv')
+
+    for country in countries:
+        learner = Learner(country, loss, startdate, predict_range, s_0, i_0, r_0)
+        # try:
+        learner.train()
+        # except BaseException:
+        #    print('WARNING: Problem processing ' + str(country) +
+        #        '. Be sure it exists in the data exactly as you entry it.' +
+        #        ' Also check date format if you passed it as parameter.')
+
+
+
+
+
+if download:
+    data_d = load_json("./data_url.json")
+    download_data(data_d)
+
+remove_province('data/time_series_19-covid-Confirmed.csv', 'data/time_series_19-covid-Confirmed-country.csv')
+remove_province('data/time_series_19-covid-Recovered.csv', 'data/time_series_19-covid-Recovered-country.csv')
+remove_province('data/time_series_19-covid-Deaths.csv', 'data/time_series_19-covid-Deaths-country.csv')
+
+for country in countries:
+    learner = Learner(country, loss, s_0=countries_populations[country], i_0=10, r_0=5)
+    # try:
+    df = learner.train()
+
+df = pd.read_csv('data/time_series_19-covid-Deaths-country.csv')

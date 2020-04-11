45ed8fb051ac04b80a84be80dc23733a21356cbf
rafael_valero <rafaelvalerofernandez@gmail.com>
2020-04-11 15:54:04 +0100

    get ready arima app

--- analysis/download_data.py
+++ analysis/download_data.py
@@ -6,7 +6,6 @@ sys.path.insert(0, '../../../covid_forcast')
 # to download the data, you need to have a data folder
 download_csv_from_link()
 
-
+download_csv_from_link(url=url, dowload_folder_name='../data/data2.csv')
 # Getting data from John Hopking repo
 url = 'https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv'
-download_csv_from_link(url=url, dowload_folder_name='../data/data2.csv')
